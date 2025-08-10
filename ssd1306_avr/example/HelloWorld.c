aedb4f8e04407760d8150dd980f978574104e21a
Kander <jansa063@gmail.com>
2025-08-10 13:43:49 +0200

    Update HelloWorld.c

--- ssd1306_avr/example/HelloWorld.c
+++ ssd1306_avr/example/HelloWorld.c
@@ -1,18 +1,20 @@
-#define F_CPU 16000000UL //Define your XCLK
+#define F_CPU 16000000UL
 
 #include <avr/io.h>
 #include <util/delay.h>
-#include "../ssd1306.h"
+#include "ssd1306.h"
+#include<string.h>
 
 int main(void) {
     spi_init();
     ssd1306_init();
-    ssd1306_clear();
-    
-    ssd1306_draw_string(0, 0, " Hello World!");
-    
-    while (1) {
 
+    ssd1306_clear_buffer();
+    ssd1306_display();
+    while (1) {
+        ssd1306_draw_string_buffer(1, 1, " Hello World");
+        ssd1306_display();
+        _delay_ms(3000);
+        ssd1306_clear_buffer();
     }
-    return 0;
 }
